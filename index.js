cb8674d35b872b5952ffdb9a6e969a9ebc918987
Kay Otiq <lyndamontanez@gmail.com>
2019-10-21 22:08:07 -0700

    feature: Implementing versioning

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const child = require("child_process");
 const fs = require("fs");
 
+const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
 const output = child
-  .execSync(`git log --format=%B%H----DELIMITER----`)
+  .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
   .toString("utf-8");
 
 const commitsArray = output
